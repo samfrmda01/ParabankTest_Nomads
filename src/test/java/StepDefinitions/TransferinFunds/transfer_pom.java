9f8f93cf8af042c91a6d65805af030cc6d4dfad0
ozcansayis <ozcansayis@gmail.com>
2024-04-06 04:03:59 +0300

    US_606's POM elements added(v1)

--- src/test/java/StepDefinitions/TransferinFunds/transfer_pom.java
+++ src/test/java/StepDefinitions/TransferinFunds/transfer_pom.java
@@ -1,4 +1,36 @@
 package StepDefinitions.TransferinFunds;
 
-public class transfer_pom {
+import Pages.ParentPage;
+import Utilities.GWD;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.*;
+
+import java.util.List;
+
+public class transfer_pom extends ParentPage {
+    public transfer_pom() {
+        PageFactory.initElements(GWD.getDriver(), this);
+    }
+    // login
+    @FindBy(name = "username")
+    public WebElement username;
+
+    @FindBy(name = "password")
+    public WebElement password;
+
+    @FindBy(css = "[value='Log In']")
+    public WebElement loginButton;
+
+    // left nav
+    @FindBy(linkText = "Accounts Overview")
+    public WebElement accountsOverview;
+
+    @FindBy(linkText = "Transfer Funds")
+    public WebElement transferFunds;
+
+    // dialog content
+    @FindBy(css = "tr[class='ng-scope']")
+    public List<WebElement> accounts;
+
+
 }
