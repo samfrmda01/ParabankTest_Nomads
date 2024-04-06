ccd172a8ede25dfff38c46d2841f530b4f204eb7
Abdulkabir <turkogluabdulkabir@gmail.com>
2024-04-07 02:57:08 +0300

    Created register and login

--- src/test/java/StepDefinitions/User_login/login_pom.java
+++ src/test/java/StepDefinitions/User_login/login_pom.java
@@ -1,4 +1,24 @@
 package StepDefinitions.User_login;
 
-public class login_pom {
+import Pages.ParentPage;
+import Utilities.GWD;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class login_pom extends ParentPage {
+    public login_pom() {
+        PageFactory.initElements(GWD.getDriver(), this);
+    }
+
+    @FindBy(css = "input[name='username']")
+    WebElement username;
+    @FindBy(css = "input[name='password']")
+    WebElement password;
+    @FindBy(css = "input[type='submit']")
+    WebElement loginButton;
+    @FindBy(css = "p[class='smallText']+h2")
+    WebElement sonuc;
+    @FindBy(css = "div[id='rightPanel']>p")
+    WebElement sonuc2;
 }
