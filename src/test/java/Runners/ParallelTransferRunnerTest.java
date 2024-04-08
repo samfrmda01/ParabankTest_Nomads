759df1a5ab432a6b0a02a126fdad699b6c395223
ozcansayis <ozcansayis@gmail.com>
2024-04-08 03:19:32 +0300

    Parallel runner test created for TC_606

---
+++ src/test/java/Runners/ParallelTransferRunnerTest.java
@@ -0,0 +1,19 @@
+package Runners;
+
+import Utilities.GWD;
+import io.cucumber.testng.*;
+import org.testng.annotations.*;
+
+@CucumberOptions(
+        tags = "@Payment and @Regression",
+        features = {"src/test/java/FeatureFiles/transfer.feature"},
+        glue = {"StepDefinitions"},
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+)
+public class ParallelTransferRunnerTest extends AbstractTestNGCucumberTests {
+    @BeforeClass
+    @Parameters("browserType")
+    public void beforeClass(String browserName) {
+        GWD.threadBrowserName.set(browserName);
+    }
+}
