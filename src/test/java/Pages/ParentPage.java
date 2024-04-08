207416224a2eb5b9e2b25c78752085d6ea197b54
Samed <158621914+samfrmda01@users.noreply.github.com>
2024-04-08 23:40:10 +0300

    Update ParentPage.java

--- src/test/java/Pages/ParentPage.java
+++ src/test/java/Pages/ParentPage.java
@@ -35,9 +35,6 @@ public class ParentPage {
 
     public void verifyContainsText(WebElement element, String value) {
         wait.until(ExpectedConditions.textToBePresentInElement(element, value));
-
-      
-
         Assert.assertTrue(element.getText().toLowerCase().contains(value.toLowerCase()),"The message does not contain the expected value!");
     }
 
