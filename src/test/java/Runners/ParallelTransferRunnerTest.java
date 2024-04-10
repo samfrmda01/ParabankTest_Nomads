ac0352089fe49717de06d3f1f172b9f91efbfd9a
Samed <samfromda01@hotmail.com>
2024-04-10 19:22:59 +0300

    new updates

--- src/test/java/Runners/ParallelTransferRunnerTest.java
+++ src/test/java/Runners/ParallelTransferRunnerTest.java
@@ -2,18 +2,18 @@ package Runners;
 
 import Utilities.GWD;
 import io.cucumber.testng.*;
-import org.testng.annotations.*;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 @CucumberOptions(
-        tags = "@Payment and @Regression",
         features = {"src/test/java/FeatureFiles/transfer.feature"},
         glue = {"StepDefinitions"},
         plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 )
 public class ParallelTransferRunnerTest extends AbstractTestNGCucumberTests {
-//    @BeforeClass // for parallel test
-//    @Parameters("browserType")
-//    public void beforeClass(String browserName) {
-//        GWD.threadBrowserName.set(browserName);
-//    }
+    @BeforeClass // for parallel test
+    @Parameters("browserType")
+    public void beforeClass(String browserName) {
+        GWD.threadBrowserName.set(browserName);
+    }
 }
